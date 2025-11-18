37f297705727a8f38e83deed291947a53983cc2f
Martin Rossakov <mstislavlandsh@gmail.com>
2025-11-18 09:35:05 +0200

    git käsud on kirjutatud

--- gitKasutamine.php
+++ gitKasutamine.php
@@ -27,3 +27,21 @@ echo "</li>";
 </li>
 <?php
 echo "<li>GitHub projektiga sidumine";
+echo "<pre>";
+echo "git remote add origin git@github.com:painkiller102k/veebPHP.git;
+
+git branch -M main;
+
+git push -u origin main";
+
+echo "</pre>";
+echo "</li>";
+
+echo "<li>Projekti kloonimine desktopi.<br>
+    * Kontrolli, et id_rsa key on olemas .ssh kaustus<br>
+    * GIT CMD on lahti ja <br>";
+echo "<pre>";
+echo "git clone git@github.com:painkiller102k/veebPHP.git;";
+echo "</pre>";
+echo "</li>";
+echo "</ol>";
