93f70d5e07d0ed4caac52c17e2cd46dbf33c50ab
Martin Rossakov <103848033+painkiller102k@users.noreply.github.com>
2025-11-25 11:18:49 +0200

    uue nav menu

--- nav.php
+++ nav.php
@@ -1,12 +1,26 @@
-<nav>
-    <ul>
-        <li><a href="?link=avaleht.php">Avaleht</a></li>
-        <li><a href="?link=gitKasutamine.php">GIT käsud</a></li>
-        <li><a href="?link=kalkulaator.php">Joogikalkulaator JS Ülesanne</a></li>
-        <li><a href="?link=muusikakusitlus.php">Muusika küsitlus JS Ülesanne</a></li>
-        <li><a href="?link=ajaFunktsioonid.php">Ajafunktsioonid</a></li>
-        <li><a href="?link=tekstFunktsioonid.php">Tekst funktsioonid</a></li>
-        <li><a href="?link=matemFunktsioonid.php">Matemaatika funktsioonid</a></li>
-        <li><a href="https://martinrossakov24.thkit.ee/" target="_blank">Vana Index</a></li>
-    </ul>
-</nav>
+<div id="main">
+    <div id="content">
+        <nav class="menu">
+            <ul>
+                <li><a href="?link=avaLeht.php">Avaleht</a></li>
+                <li><a href="?link=gitKasutamine.php">GIT käsud</a></li>
+
+                <li><a>JS Ülesanded</a>
+                    <ul class="dropdown">
+                        <li><a href="?link=kalkulaator.php">Joogikalkulaator JS Ülesanne</a></li>
+                        <li><a href="?link=muusikakusitlus.php">Muusika küsitlus JS Ülesanne</a></li>
+                    </ul>
+                </li>
+
+                <li><a>PHP Funktsioonid</a>
+                    <ul class="dropdown">
+                        <li><a href="?link=ajaFunktsioonid.php">Ajafunktsioonid</a></li>
+                        <li><a href="?link=tekstFunktsioonid.php">Tekst funktsioonid</a></li>
+                        <li><a href="?link=matemFunktsioonid.php">Matemaatika funktsioonid</a></li>
+                    </ul>
+                </li>
+                <li><a href="https://martinrossakov24.thkit.ee/" target="_blank">Vana Index</a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
