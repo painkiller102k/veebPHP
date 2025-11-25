dcc3f6956b913e2486040c90c644fb28fd325f79
Martin Rossakov <103848033+painkiller102k@users.noreply.github.com>
2025-11-25 09:49:45 +0200

    nav uuendamine

--- nav.php
+++ nav.php
@@ -6,6 +6,7 @@
         <li><a href="?link=muusikakusitlus.php">Muusika küsitlus JS Ülesanne</a></li>
         <li><a href="?link=ajaFunktsioonid.php">Ajafunktsioonid</a></li>
         <li><a href="?link=tekstFunktsioonid.php">Tekst funktsioonid</a></li>
+        <li><a href="?link=matemFunktsioonid.php">Matemaatika funktsioonid</a></li>
         <li><a href="https://martinrossakov24.thkit.ee/" target="_blank">Vana Index</a></li>
     </ul>
 </nav>
