f6ed53bcad6f7c5caa5fc58dca6322b9ee495b16
Martin Rossakov <103848033+painkiller102k@users.noreply.github.com>
2025-11-27 17:02:05 +0200

    tunniplaan konspekt done

---
+++ content/kosnpekt/neljapaev.php
@@ -0,0 +1,10 @@
+<?php require("p2is.php"); ?>
+    <h2>Neljapäev</h2>
+    <p>
+    <ol>
+        <li>Emakeel</li>
+        <li>Ajalugu</li>
+        <li>Matemaatika</li>
+    </ol>
+    </p>
+<?php require("jalus.php"); ?>
